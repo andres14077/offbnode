00ca81027570a6ed64b0346f6671494addf26987
Andrés Alejandro  Moreno Sánchez <andresalemosan@hotmail.com>
2022-08-29 16:15:33 -0500

    añadido nodo para realizar rutas cuadradas

---
+++ src/offb_node_rute_square.cpp
@@ -0,0 +1,90 @@
+
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_datatypes.h>
+#include <string.h>
+
+
+// configuracion de la camara
+double Width_sensor=6.17;               // en mm
+double Height_sensor=4.63;              // en mm
+double Image_pix_Width=4608;
+double Image_pix_Height=3456;
+double Focal_length=4.9;                // en mm
+
+
+double mavros_msgs::State current_state;
+void state_cb(const mavros_msgs::State::ConstPtr& msg){
+    current_state = *msg;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "offb_node_rute_square");
+    ros::NodeHandle nh;
+
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
+            ("mavros/state", 10, state_cb);
+    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
+            ("mavros/setpoint_position/local", 10);
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
+            ("mavros/cmd/arming");
+    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
+            ("mavros/set_mode");
+
+    //the setpoint publishing rate MUST be faster than 2Hz
+    ros::Rate rate(20.0);
+
+    // wait for FCU connection
+    while(ros::ok() && !current_state.connected){
+        ros::spinOnce();
+        rate.sleep();
+    }
+    geometry_msgs::PoseStamped pose;
+    double H = atof(argv[1]);
+    pose.pose.position.x = 0;
+    pose.pose.position.y = 0;
+    pose.pose.position.z = H;
+    pose.pose.orientation=tf::createQuaternionMsgFromYaw(atan2(1,0));
+
+    ROS_INFO("x=[%f],y=[%f],z=[%f]",pose.pose.position.x,pose.pose.position.y,pose.pose.position.z);
+    double GSD=(Width_sensor*H*100)/(Focal_length*Image_pix_Width);
+
+    mavros_msgs::SetMode offb_set_mode;
+    offb_set_mode.request.custom_mode = "OFFBOARD";
+
+    mavros_msgs::CommandBool arm_cmd;
+    arm_cmd.request.value = true;
+
+    ros::Time last_request = ros::Time::now();
+
+    while(ros::ok()){
+        if( current_state.mode != "OFFBOARD" &&
+            (ros::Time::now() - last_request > ros::Duration(5.0))){
+            if( set_mode_client.call(offb_set_mode) &&
+                offb_set_mode.response.mode_sent){
+                ROS_INFO("Offboard enabled");
+            }
+            last_request = ros::Time::now();
+        } else {
+            if( !current_state.armed &&
+                (ros::Time::now() - last_request > ros::Duration(5.0))){
+                if( arming_client.call(arm_cmd) &&
+                    arm_cmd.response.success){
+                    ROS_INFO("Vehicle armed");
+                }
+                last_request = ros::Time::now();
+            }
+        }
+
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
+}
