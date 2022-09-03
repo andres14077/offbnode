b0cb682210e1a3ea4694870dae00c2e4681380ab
Andrés Alejandro  Moreno Sánchez <andresalemosan@hotmail.com>
2022-09-03 15:03:41 -0500

    Prueba ruta de vuelo

--- src/offb_node_rute_square.cpp
+++ src/offb_node_rute_square.cpp
@@ -1,5 +1,6 @@
 
 #include <ros/ros.h>
+#include <nav_msgs/Path.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
@@ -7,7 +8,7 @@
 #include <tf/transform_listener.h>
 #include <tf/transform_datatypes.h>
 #include <string.h>
-
+#include <math.h>
 
 // configuracion de la camara
 double Width_sensor=6.17;               // en mm
@@ -15,20 +16,70 @@ double Height_sensor=4.63;              // en mm
 double Image_pix_Width=4608;
 double Image_pix_Height=3456;
 double Focal_length=4.9;                // en mm
+// configuracion de parametros de vuelo
+double H=50;                            // en m
+double translape_lateral=70;            // en %
+double translape_longitudinal=70;       // en %
+double angulo_entrada=70;               // en grados
+double distancia_respuesta=20;               // en m
+double gradiente_x=0;                    // en m
+double gradiente_y=0;                    // en m
+double gradiente_z=0;                    // en m
+// cerca de vuelo
+double min_x=-300;                    // en m desde el punto de partida
+double min_y=-300;                    // en m desde el punto de partida
+double max_x=300;                    // en m desde el punto de partida
+double max_y=300;                    // en m desde el punto de partida
 
+double distancia(double l_x,double l_y,double l_z,double s_x,double s_y,double s_z ){
+    double dx=(l_x-s_x)*(l_x-s_x);
+    double dy=(l_y-s_y)*(l_y-s_y);
+    double dz=(l_z-s_z)*(l_z-s_z);
+    return sqrt(dx+dy+dz);
+}
 
 mavros_msgs::State current_state;
 void state_cb(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
 }
 
+geometry_msgs::PoseStamped  current_local_pose;
+void local_pose_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
+    current_local_pose = *msg;
+}
+
 int main(int argc, char **argv)
 {
+    //double H = atof(argv[1]);
+    //calculo de variables de vuelo
+    double GSD=(Width_sensor*H*100)/(Focal_length*Image_pix_Width);             // en cm/pix
+
+
+
     ros::init(argc, argv, "offb_node_rute_square");
     ros::NodeHandle nh;
 
+    ros::Publisher nav_pos_pub = nh.advertise<nav_msgs::Path>
+            ("iris/nav", 10);
+    nav_msgs::Path path;
+    for (int i = 0; i < 100; ++i)
+    {
+        geometry_msgs::PoseStamped pose;
+        pose.header.stamp = i;
+        pose.header.frame_id = "map";
+        pose.pose.position.x = 10*cos(62.8318e-3*i);
+        pose.pose.position.y = 10*sin(62.8318e-3*i);
+        pose.pose.position.z = 10*cos(62.8318e-3*i);
+        pose.pose.orientation=tf::createQuaternionMsgFromYaw(atan2(pose.pose.position.y,pose.pose.position.x));
+        path.poses.push_back(pose);
+    }
+    nav_pos_pub.publish(path);
+
+    return 0;
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
             ("mavros/state", 10, state_cb);
+    ros::Subscriber local_pose_sub = nh.subscribe<geometry_msgs::PoseStamped>
+            ("mavros/local_position/pose", 10, local_pose_cb);
     ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
             ("mavros/setpoint_position/local", 10);
     ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
@@ -44,15 +95,11 @@ int main(int argc, char **argv)
         ros::spinOnce();
         rate.sleep();
     }
-    geometry_msgs::PoseStamped pose;
-    double H = atof(argv[1]);
+    //geometry_msgs::PoseStamped pose;
     pose.pose.position.x = 0;
     pose.pose.position.y = 0;
     pose.pose.position.z = H;
     pose.pose.orientation=tf::createQuaternionMsgFromYaw(atan2(1,0));
-
-    ROS_INFO("x=[%f],y=[%f],z=[%f]",pose.pose.position.x,pose.pose.position.y,pose.pose.position.z);
-    double GSD=(Width_sensor*H*100)/(Focal_length*Image_pix_Width);
 
     mavros_msgs::SetMode offb_set_mode;
     offb_set_mode.request.custom_mode = "OFFBOARD";
@@ -81,6 +128,15 @@ int main(int argc, char **argv)
             }
         }
 
+            // pose.pose.position.x = 0;
+            // pose.pose.position.y = 0;
+            // pose.pose.position.z = H;
+            // pose.pose.orientation=tf::createQuaternionMsgFromYaw(atan2(1,0));
+        ROS_INFO("set point x=[%f],y=[%f],z=[%f]",pose.pose.position.x,pose.pose.position.y,
+            pose.pose.position.z);
+        ROS_INFO("local point x=[%f],y=[%f],z=[%f]",current_local_pose.pose.position.x,
+            current_local_pose.pose.position.y,current_local_pose.pose.position.z);
+    
         local_pos_pub.publish(pose);
         ros::spinOnce();
         rate.sleep();
