b9ebd43de210bfc3a48c85354c98d5c2def3b9f1
Andrés Alejandro  Moreno Sánchez <andresalemosan@hotmail.com>
2022-09-21 11:07:48 -0500

    .

--- source/Descargar_models.sh
+++ source/Descargar_models.sh
@@ -11,4 +11,3 @@ cd $dic
 wget -c --no-check-certificate 'https://drive.google.com/uc?export=download&id=1x_1SKbk1qCJp0v7YT7D5XzSQGnussz4i&confirm=t' -O modelos.tar.gz
 tar -xzvf modelos.tar.gz
 rm -f modelos.tar.gz
-exit 0
--- source/Descargar_models_light.sh
+++ source/Descargar_models_light.sh
@@ -11,4 +11,3 @@ cd $dic
 wget -c --no-check-certificate 'https://drive.google.com/uc?export=download&id=1S4KLVSew-6n2qKmZMl_saBsk_0gFZxbJ&confirm=t' -O modelos.tar.gz
 tar -xzvf modelos.tar.gz
 rm -f modelos.tar.gz
-exit 0
