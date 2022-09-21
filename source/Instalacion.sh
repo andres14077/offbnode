9ec3cdf763f80e47c74d9f8724738179e66fe1d0
Andrés Alejandro  Moreno Sánchez <andresalemosan@hotmail.com>
2022-09-21 18:00:12 -0500

    Update Instalacion.sh

--- source/Instalacion.sh
+++ source/Instalacion.sh
@@ -8,7 +8,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt install -y curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo apt install -y ros-melodic-desktop
+sudo apt install -y ros-melodic-desktop-full
 
 
 sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
