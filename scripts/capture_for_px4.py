f77a545a618737070657c4236488840295787bb0
Andrés Alejandro  Moreno Sánchez <andresalemosan@hotmail.com>
2022-09-22 20:28:07 -0700

    Update capture_for_px4.py

--- scripts/capture_for_px4.py
+++ scripts/capture_for_px4.py
@@ -5,7 +5,7 @@ from mavros_msgs.msg import WaypointReached
 from mavros_msgs.msg import MountControl
 from sensor_msgs.msg import Image
 
-from cv_bridge import CvBridge 
+from cv_bridge import CvBridge
 import cv2
 #import tensorflow as tf
 import sys
@@ -13,7 +13,7 @@ import os
 
 
 class Cap_imag:
-    
+
     def __init__(self):
         rospy.loginfo("init node capture images for offboard")
         rospy.Subscriber("/offboard/mission/reached",WaypointReached,self.Waypoint_Reached_Calback)
@@ -23,12 +23,16 @@ class Cap_imag:
         self.capture_image=0
         self.pub=rospy.Publisher('/mavros/mount_control/command', MountControl, queue_size=10)
         self.cmd=MountControl()
+        self.cmd.header.frame_id="map"
+        self.cmd.mode=2
+        self.cmd.pitch=0
+
         self.image_directory=sys.argv[1]
+
         if not (os.path.exists(self.image_directory)):
             rospy.logerr("Carpeta no encontrada:"+self.image_directory)
             exit()
-            
-                                        
+
     def Image_Calback(self,image):
         if self.capture_image:
             self.capture_image=0
@@ -37,18 +41,15 @@ class Cap_imag:
             name=self.image_directory+"/Captura_No_"+str(self.last_id)+".png"
             cv2.imwrite(name,cv_image)
             rospy.loginfo("captura de imagen "+str(self.last_id))
-    
+
     def Waypoint_Reached_Calback(self,waypoint):
         if((self.last_id!=waypoint.wp_seq) and (self.capture_image==0)):
             self.last_id=waypoint.wp_seq
             self.capture_image=1
     def upload(self):
         self.cmd.header.stamp=rospy.Time.now()
-        self.cmd.header.frame_id="map"
-        self.cmd.mode=2
-        self.cmd.pitch=0
         self.pub.publish(self.cmd)
-        
+
 def capture_for_px4():
     rospy.init_node('capture_for_px4', anonymous=True)
     cap=Cap_imag()
@@ -56,8 +57,8 @@ def capture_for_px4():
     while not rospy.is_shutdown():
         cap.upload()
         rate.sleep()
-    
-    
+
+
 if __name__ == '__main__':
     try:
         capture_for_px4()
@@ -68,5 +69,5 @@ if __name__ == '__main__':
         rospy.logerr("Error desconocido")
         exit()
 
-    
-    
+
+
