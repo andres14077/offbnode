255282fabd72758a6120698c87e22bf7128eafaf
Andrés Alejandro  Moreno Sánchez <andresalemosan@hotmail.com>
2022-12-05 15:39:58 -0500

    .

--- scripts/procesado_plc.py
+++ scripts/procesado_plc.py
@@ -5,9 +5,11 @@ import sensor_msgs.point_cloud2 as pc2
 from sensor_msgs.msg import PointCloud2
 import matplotlib.pyplot as plt
 distancia_z=[]
+t=[]
 
 def point_cloud_cb(msg):
     global distancia_z
+    global t
     z_mean=0
     i=0
     for p in pc2.read_points(msg, field_names = ("z"), skip_nans=True):
@@ -15,6 +17,8 @@ def point_cloud_cb(msg):
         i+=1
     rospy.loginfo("z_mean : %f",z_mean/i)
     distancia_z.append(z_mean/i)
+    tnow=rospy.Time.now()
+    t.append(tnow.secs)
 
 if __name__ == '__main__':
     rospy.init_node('servicio_de_kill_node', anonymous=True)
@@ -23,6 +27,7 @@ if __name__ == '__main__':
 
 
     while(not rospy.is_shutdown()):
-        plt.scatter(distancia_z)
+        
+        plt.plot(distancia_z)
         plt.pause(0.0001)
         rate.sleep()
