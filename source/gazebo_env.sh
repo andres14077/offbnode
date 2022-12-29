9c05ffe6427c89bc9241f6e90300b8e39090e310
Andres-A-Moreno-S <andres.moreno@digitalcomtech.com>
2022-12-29 15:14:39 -0500

    gazebo prueba

--- source/gazebo_env.sh
+++ source/gazebo_env.sh
@@ -2,6 +2,7 @@
 #
 pushd ~/offbnode
 export GAZEBO_MASTER_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker-compose ps -q ros_melodic_px4))
+#export GAZEBO_MASTER_IP=192.168.1.101
 export GAZEBO_MASTER_URI=$(echo $GAZEBO_MASTER_IP):11345
 popd
 
