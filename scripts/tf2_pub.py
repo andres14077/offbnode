421000c300fa2fb78b21b392872394f77d0719b3
Andres-A-Moreno-S <andres.moreno@digitalcomtech.com>
2022-12-29 17:59:21 -0500

    tf2

---
+++ scripts/tf2_pub.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import rospy
+
+# Because of transformations
+import tf_conversions
+
+import tf2_ros
+import geometry_msgs.msg
+from nav_msgs.msg import Odometry
+
+def tf_callback(msg, turtlename):
+    br = tf2_ros.TransformBroadcaster()
+    t = geometry_msgs.msg.TransformStamped()
+
+    t.header.stamp = rospy.Time.now()
+    t.header.frame_id = msg.frame_id
+    t.child_frame_id = turtlename
+    t.transform.translation.x = msg.x
+    t.transform.translation.y = msg.y
+    t.transform.translation.z = 0.0
+    q = tf_conversions.transformations.quaternion_from_euler(0, 0, msg.theta)
+    t.transform.rotation.x = q[0]
+    t.transform.rotation.y = q[1]
+    t.transform.rotation.z = q[2]
+    t.transform.rotation.w = q[3]
+
+    br.sendTransform(t)
+
+if __name__ == '__main__':
+    rospy.init_node('tf2_pub')
+    rospy.rospy.Subscriber("mavros/global_position/local", Odometry, tf_callback, queue_size=5)
+    rospy.spin()
