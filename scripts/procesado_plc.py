60d6335d1241bf268578039dfc1c6c7daf55daf3
Andrés Alejandro  Moreno Sánchez <andresalemosan@hotmail.com>
2023-01-11 23:01:53 -0500

    Update procesado_plc.py

--- scripts/procesado_plc.py
+++ scripts/procesado_plc.py
@@ -4,22 +4,20 @@ import rospy
 import sensor_msgs.point_cloud2 as pc2
 from sensor_msgs.msg import PointCloud2
 import matplotlib.pyplot as plt
+import statistics
 distancia_z=[]
 t=[]
-
 def point_cloud_cb(msg):
     global distancia_z
     global t
-    z_mean=0
-    i=0
     z=[]
-    for p in pc2.read_points(msg, field_names = ("z"), skip_nans=True):
-        z_mean += p[0]
-        z.append(p)
-        i+=1
-    print (z)
-    rospy.loginfo("z_mean : %f",z_mean/i)
-    distancia_z.append(z_mean/i)
+    for p in pc2.read_points(msg, field_names = ("x", "y", "z"), skip_nans=True):
+        z.append(p[2])
+    st_dev = statistics.pstdev(z)
+    print (st_dev)
+    dist=statistics.mean(z)
+    rospy.loginfo("z_mean : %f",dist)
+    distancia_z.append(dist)
     tnow=rospy.Time.now()
     t.append(tnow.secs)
 
