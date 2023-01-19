12d9a20e8e316e2a1b801379af8ce8819d6b54e6
Andres-A-Moreno-S <andres.moreno@digitalcomtech.com>
2023-01-19 12:20:07 -0500

    up

--- scripts/procesado_plc_2.py
+++ scripts/procesado_plc_2.py
@@ -18,14 +18,16 @@ class procesado_plc_2:
         self.vector_sub=rospy.Subscriber('offbnode/vector_in_plane', Vector3Stamped, self.vector_cb)
         self.tf_buffer = tf2_ros.Buffer(rospy.Duration(100.0))  # tf buffer length
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
-        self.t = TransformStamped()
         self.br = tf2_ros.TransformBroadcaster()
+
+        self.t = TransformStamped()
+        self.t.transform.rotation.w = 1
         self.t.header.frame_id = "cgo3_camera_optical_link"
         self.t.child_frame_id = "plano_terreno"
     def point_cb(self,msg):
-        self.t.translation.x = msg.point.x
-        self.t.translation.y = msg.point.y
-        self.t.translation.z = msg.point.z
+        self.t.transform.translation.x = msg.point.x
+        self.t.transform.translation.y = msg.point.y
+        self.t.transform.translation.z = msg.point.z
     def vector_cb(self,msg):
         v_len=math.sqrt(msg.vector.x*msg.vector.x + msg.vector.y*msg.vector.y + msg.vector.z*msg.vector.z)
         pitch = math.asin(-msg.vector.y/v_len);
