f157a6665fbd6029315412815c8e171ccf14b026
Andres-A-Moreno-S <andres.moreno@digitalcomtech.com>
2023-01-19 11:51:55 -0500

    Update procesado_plc_2.py

--- scripts/procesado_plc_2.py
+++ scripts/procesado_plc_2.py
@@ -1,36 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import rospy
+import math
 from geometry_msgs.msg import Vector3Stamped
 from geometry_msgs.msg import PointStamped
 from geometry_msgs.msg import TransformStamped
 import tf2_ros
 import tf2_geometry_msgs
-from tf2_geometry_msgs.msg import Vector3
 from tf.transformations import euler_from_quaternion
+from tf.transformations import quaternion_from_euler
 
 class procesado_plc_2:
     def __init__(self):
         rospy.init_node('procesado_plc_2', anonymous=True)
         self.rate=rospy.Rate(20)
-        self.point_sub=rospy.Subscriber('offbnode/point_in_plane', PointStamped, queue_size=10)
-        self.vector_sub=rospy.Subscriber('offbnode/vector_in_plane', Vector3Stamped, queue_size=10)
+        self.point_sub=rospy.Subscriber('offbnode/point_in_plane', PointStamped, self.point_cb )
+        self.vector_sub=rospy.Subscriber('offbnode/vector_in_plane', Vector3Stamped, self.vector_cb)
         self.tf_buffer = tf2_ros.Buffer(rospy.Duration(100.0))  # tf buffer length
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
-    def point_cloud_cb(self,msg):
-        vector_normal=Vector3Stamped()
-        vectorN=Vector3()
-        point_plane=PointStamped()
-        point_plane.header.frame_id="cgo3_camera_optical_link"
-        point_plane.header.stamp = rospy.Time.now()
-        point_plane.point.z = b[0]
-
-        vector_normal.header.frame_id="cgo3_camera_optical_link"
-        vector_normal.header.stamp = rospy.Time.now()
-        vector_normal.vector.x = b[1]*-1
-        vector_normal.vector.y = b[2]*-1
-        vector_normal.vector.z = 1
-
+        self.t = TransformStamped()
+        self.t.header.frame_id = "cgo3_camera_optical_link"
+        self.t.child_frame_id = "plano_terreno"
+    def point_cb(self,msg):
+        self.t.translation.x = msg.point.x
+        self.t.translation.y = msg.point.y
+        self.t.translation.z = msg.point.z
+    def vector_cb(self,msg):
+        v_len=math.sqrt(msg.vector.x*msg.vector.x + msg.vector.y*msg.vector.y + msg.vector.z*msg.vector.z)
+        pitch = math.asin(-msg.vector.y/v_len);
+        yaw = math.atan2(msg.vector.x/v_len, msg.vector.z/v_len)
+        q=quaternion_from_euler(0,pitch,yaw)
+        self.t.transform.rotation.x = q[0]
+        self.t.transform.rotation.y = q[1]
+        self.t.transform.rotation.z = q[2]
+        self.t.transform.rotation.w = q[3]
 
 if __name__ == '__main__':
     
