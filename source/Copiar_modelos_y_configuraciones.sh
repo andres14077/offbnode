78ae6ca3b6b025396ac6699a8a43643bdefb0d2d
Andres-A-Moreno-S <andres.moreno@digitalcomtech.com>
2023-02-11 11:18:13 -0500

    Update Copiar_modelos_y_configuraciones.sh

--- source/Copiar_modelos_y_configuraciones.sh
+++ source/Copiar_modelos_y_configuraciones.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
+if [[ -n $(ls ~/catkin_ws/src/offbnode 2>/dev/null) ]]; then
+	dic=~/catkin_ws/src/offbnode
+elif [[ -n $(ls ~/offbnode 2>/dev/null) ]]; then
+	dic=~/offbnode
+else
+	echo "carpeta offbnode no encontrada"
+	exit 1
+fi
 ##copiar achivos
-cp -r ~/catkin_ws/src/offbnode/models/* ~/Firmware/Tools/sitl_gazebo/models
-cp -r ~/catkin_ws/src/offbnode/init.d/* ~/Firmware/ROMFS/px4fmu_common/init.d-posix
+cp -rv $dic/models/* ~/Firmware/Tools/sitl_gazebo/models
+cp -rv $dic/init.d/* ~/Firmware/ROMFS/px4fmu_common/init.d-posix
 
