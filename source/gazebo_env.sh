76365c78ec482c2badfde5affadf808892074984
Andres-A-Moreno-S <andres.moreno@digitalcomtech.com>
2023-03-12 17:53:31 -0500

    Update gazebo_env.sh

--- source/gazebo_env.sh
+++ source/gazebo_env.sh
@@ -11,3 +11,5 @@ source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
 popd
+
+export ROS_MASTER_URI=http://$(echo $GAZEBO_MASTER_IP):11311/
