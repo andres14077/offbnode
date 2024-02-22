b2ebc920efdf7aa9118c212325992dde38d075db
Andres-A-Moreno-S <andres.moreno@digitalcomtech.com>
2024-02-22 08:03:01 -0500

    Update Instalacion jetson.sh

--- source/Instalacion jetson.sh
+++ source/Instalacion jetson.sh
@@ -9,13 +9,14 @@ sudo apt-get install -y curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt-get update
 
-sudo apt-get install -y git python3-dev python3-numpy python3-py python3-pytest python3-opencv python3-pip ros-melodic-base python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential libglew-dev libtiff5-dev zlib1g-dev libjpeg-dev libavcodec-dev libavformat-dev libavutil-dev libpostproc-dev libswscale-dev libeigen3-dev libtbb-dev libgtk2.0-dev pkg-config python-dev python-numpy python-py python-pytest python-pip python-jinja2 ros-melodic-control* ros-melodic-transmission-interface ros-melodic-joint-limits-interface ros-melodic-mav*
-
+sudo apt-get install -y git python3-dev python3-numpy python3-py python3-pytest python3-opencv python3-pip ros-melodic-base python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential libglew-dev libtiff5-dev zlib1g-dev libjpeg-dev libavcodec-dev libavformat-dev libavutil-dev libpostproc-dev libswscale-dev libeigen3-dev libtbb-dev libgtk2.0-dev pkg-config python-dev python-numpy python-py python-pytest python-pip python-jinja2 ros-melodic-control* ros-melodic-transmission-interface ros-melodic-joint-limits-interface ros-melodic-mav* libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
 
 pip install numpy toml
 pip3 install --upgrade pip
-pip3 install catkin_pkg rospkg tensorflow scikit-learn seaborn
-
+pip3 install catkin_pkg rospkg scikit-learn seaborn testresources setuptools
+sudo ln -s /usr/include/locale.h /usr/include/xlocale.h
+pip3 install -U future mock keras_preprocessing keras_applications gast protobuf pybind11 cython pkgconfig packaging
+pip3 install --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v461 tensorflow
 
 # Actualizar gazebo
 sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
@@ -45,33 +46,33 @@ catkin_make
 
 source ~/catkin_ws/devel/setup.bash
 
-cd
-mkdir opencv3
-cd opencv3
+# cd
+# mkdir opencv3
+# cd opencv3
 
-## Clonar repositorio
-git clone https://github.com/opencv/opencv.git opencv
-cd opencv
-git checkout 3.3.1
-cd ..
-## OpenCV Extras
-git clone https://github.com/opencv/opencv_extra.git
-cd opencv_extra/
-git checkout 3.3.1
-cd ..
-cd opencv
+# ## Clonar repositorio
+# git clone https://github.com/opencv/opencv.git opencv
+# cd opencv
+# git checkout 3.3.1
+# cd ..
+# ## OpenCV Extras
+# git clone https://github.com/opencv/opencv_extra.git
+# cd opencv_extra/
+# git checkout 3.3.1
+# cd ..
+# cd opencv
 
-## Crear directorio para construir los paquetes
-mkdir build
-cd build/
-## Configuracion de la comiplacion e instalacion
-export PATH=$PATH:/usr/local/cuda/bin
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr -D BUILD_PNG=OFF -D BUILD_TIFF=OFF -D BUILD_TBB=OFF -D BUILD_JPEG=OFF -D BUILD_JASPER=OFF -D BUILD_ZLIB=OFF -D BUILD_EXAMPLES=ON -D BUILD_JAVA=OFF -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=OFF -D WITH_OPENCL=OFF -D WITH_OPENMP=OFF -D WITH_FFMPEG=ON  -DWITH_GSTREAMER=OFF -DWITH_GSTREAMER_0_10=OFF -DWITH_CUDA=ON -DWITH_GTK=ON -DWITH_VTK=OFF -DWITH_TBB=ON -DWITH_1394=OFF -DWITH_OPENEXR=OFF -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0 -DCUDA_ARCH_BIN=’3.0 3.5 5.0 6.0 6.2 ’ -DCUDA_ARCH_PTX="" -DINSTALL_C_EXAMPLES=ON -DINSTALL_TESTS=OFF -DOPENCV_TEST_DATA_PATH=../opencv_extra/testdata ../opencv ..
-## Comiplar librerias con siete de los procesadores de la CPU del dispositivo
-make -j7
-## Instalar librerias
-sudo make install
+# ## Crear directorio para construir los paquetes
+# mkdir build
+# cd build/
+# ## Configuracion de la comiplacion e instalacion
+# export PATH=$PATH:/usr/local/cuda/bin
+# export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64
+# cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr -D BUILD_PNG=OFF -D BUILD_TIFF=OFF -D BUILD_TBB=OFF -D BUILD_JPEG=OFF -D BUILD_JASPER=OFF -D BUILD_ZLIB=OFF -D BUILD_EXAMPLES=ON -D BUILD_JAVA=OFF -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=OFF -D WITH_OPENCL=OFF -D WITH_OPENMP=OFF -D WITH_FFMPEG=ON  -DWITH_GSTREAMER=OFF -DWITH_GSTREAMER_0_10=OFF -DWITH_CUDA=ON -DWITH_GTK=ON -DWITH_VTK=OFF -DWITH_TBB=ON -DWITH_1394=OFF -DWITH_OPENEXR=OFF -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0 -DCUDA_ARCH_BIN=’3.0 3.5 5.0 6.0 6.2 ’ -DCUDA_ARCH_PTX="" -DINSTALL_C_EXAMPLES=ON -DINSTALL_TESTS=OFF -DOPENCV_TEST_DATA_PATH=../opencv_extra/testdata ../opencv ..
+# ## Comiplar librerias con siete de los procesadores de la CPU del dispositivo
+# make -j7
+# ## Instalar librerias
+# sudo make install
 
 cd ~/catkin_ws/src
 git clone https://github.com/ros-simulation/gazebo_ros_pkgs.git -b melodic-devel
@@ -125,7 +126,7 @@ catkin_make
 catkin_make
 
 cd ~/catkin_ws/src/offbnode
-source source/Descargar_models.sh
+# source source/Descargar_models.sh
 source source/Copiar_modelos_y_configuraciones.sh
 
 echo "pushd ~/Firmware" >> ~/.bashrc
