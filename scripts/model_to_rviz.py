fee8896d168ac2d3f8815b392548f3fe552422f7
Andres-A-Moreno-S <andres.moreno@digitalcomtech.com>
2024-05-11 12:27:35 -0500

    update

--- scripts/model_to_rviz.py
+++ scripts/model_to_rviz.py
@@ -4,10 +4,17 @@ import rospy
 from visualization_msgs.msg import Marker
 import sys
 import rospkg
+from copy import deepcopy
 
 def publish_marker():
     if (sys.argv[1] == "pruebas"):
         exit(0)
+    rospack = rospkg.RosPack()
+    archivo = rospack.get_path('offbnode')+"/models/" + sys.argv[1] + "/" + sys.argv[1] + ".sdf"
+    marker_publisher = rospy.Publisher("/big_box", Marker, queue_size=10)
+    marker2_publisher = rospy.Publisher("/big_box_2", Marker, queue_size=10)
+    marker3_publisher = rospy.Publisher("/big_box_3", Marker, queue_size=10)
+
     marker = Marker()
     marker.header.frame_id = "/map"  # El marco de referencia en el que se ubicará el modelo
     marker.header.stamp = rospy.Time.now()
@@ -30,9 +37,6 @@ def publish_marker():
     marker.mesh_resource = "package://offbnode/models/" + sys.argv[1] + "/untitled.dae"  # Ruta al archivo del modelo 3D
     marker.id = 0
 
-
-    rospack = rospkg.RosPack()
-    archivo = rospack.get_path('offbnode')+"/models/" + sys.argv[1] + "/" + sys.argv[1] + ".sdf"
     try:
         with open(archivo, 'r') as f:
             lines = f.readlines()
@@ -46,10 +50,24 @@ def publish_marker():
     except:
         pass
 
-    marker_publisher = rospy.Publisher("/big_box", Marker, queue_size=10)
+    marker2 = deepcopy(marker)
+    marker2.color.r = 0.0
+    marker2.color.g = 0.0
+    marker2.color.b = 0.0
+    marker2.mesh_resource = "package://offbnode/models/" + sys.argv[1] + "/plane.dae"  # Ruta al archivo del modelo 3D
+    marker2.pose.position.z += 5
+
+    marker3 = deepcopy(marker)
+    marker3.color.r = 0.0
+    marker3.color.g = 0.0
+    marker3.color.b = 0.0
+    marker3.mesh_resource = "package://offbnode/models/" + sys.argv[1] + "/plane2.dae"  # Ruta al archivo del modelo 3D
+
 
     while not rospy.is_shutdown():
         marker_publisher.publish(marker)
+        marker2_publisher.publish(marker2)
+        marker3_publisher.publish(marker3)
         rospy.sleep(1)
 
 if __name__ == "__main__":
